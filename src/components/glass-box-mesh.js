ccf9f3a0a9b1899cb017a25c97f9c033b8fb8dfe
juno <dghsum@gmail.com>
2024-03-20 20:59:04 +0900

    feat: draggable camera

--- src/components/glass-box-mesh.js
+++ src/components/glass-box-mesh.js
@@ -1,21 +1,43 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import {
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 import { useLoader } from '@react-three/fiber';
 import { TextureLoader } from 'three';
 
 const BoxMesh = forwardRef(({ ...props }, ref) => {
-  const activeRef = useRef(true);
+  const meshRef = useRef();
+  const [active, setActive] = useState(false);
   const colorMap = useLoader(TextureLoader, 'assets/textures/colors.png');
-  useImperativeHandle(ref, () => {
-    return {
-      mesh: ref.current,
-      active: activeRef.current,
-    };
-  });
+
+  const handlePointerOver = useCallback(() => {
+    setActive(true);
+  }, []);
+
+  const handlePointerOut = useCallback(() => {
+    setActive(false);
+  }, []);
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        mesh: meshRef.current,
+        active: active,
+      };
+    },
+    [active],
+  );
 
   return (
     <mesh
-      ref={ref}
+      ref={meshRef}
       {...props.mesh}
+      onPointerOver={handlePointerOver}
+      onPointerOut={handlePointerOut}
     >
       <boxGeometry {...props.geometry} />
       <meshPhongMaterial
