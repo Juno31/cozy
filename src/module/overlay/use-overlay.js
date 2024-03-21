cbcebc1a13fb08a4b52be71e6f3160d8be038cbb
juno <dghsum@gmail.com>
2024-03-21 11:40:49 +0900

    feat: overlay module

---
+++ src/module/overlay/use-overlay.js
@@ -0,0 +1,50 @@
+import { useContext, useEffect, useMemo, useRef, useState } from 'react';
+import OverlayController from '@/module/overlay/overlay-controller';
+import OverlayContext from '@/module/overlay/overlay-provider';
+
+let elementId = 1;
+
+const useOverlay = ({ exitOnUnmount = true } = {}) => {
+  const context = useContext(OverlayContext);
+
+  const { mount, unmount } = context;
+  const [id] = useState(() => String(elementId++));
+
+  const overlayRef = useRef(null);
+
+  useEffect(() => {
+    return () => {
+      if (exitOnUnmount) {
+        unmount?.(id);
+      }
+    };
+  }, [exitOnUnmount, id, unmount]);
+
+  return useMemo(
+    function () {
+      return {
+        open: function (overlayElement) {
+          return mount?.(
+            id,
+            <OverlayController
+              ref={overlayRef}
+              overlayElement={overlayElement}
+              onExit={() => {
+                unmount?.(id);
+              }}
+            />,
+          );
+        },
+        close: function () {
+          overlayRef?.current?.close();
+        },
+        exit: function () {
+          unmount?.(id);
+        },
+      };
+    },
+    [id, mount, unmount],
+  );
+};
+
+export default useOverlay;
